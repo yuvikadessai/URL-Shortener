d50849977af1e011d0cebb4712a636eb08c56c48
yuvikadessai <yuvikadessai05@gmail.com>
2025-09-13 00:21:09 +0530

    new file create

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mysql = require("mysql2");
+const db = require("./config/db")
+const urlRoutes = require("./routes/urlRoutes");
+const cors = require("cors");
+
+
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],        // allowed methods
+    credentials: true
+}));
+// Middleware to parse incoming data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", urlRoutes);
+
+const PORT = 8000;
+app.listen(PORT, (req, res)=>{
+   
+    console.log(`Server is running on PORT ${PORT}`) ;
+});
